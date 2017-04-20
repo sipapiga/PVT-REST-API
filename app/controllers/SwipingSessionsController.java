219c77ddee517e06a4f68683b42b68f1f1dcf60e
Simon Olofsson <s.olofsson@gmail.com>
2017-04-20 10:04:13 +0200

    Basic tests for creating new swiping sessions up and running.

--- app/controllers/SwipingSessionsController.java
+++ app/controllers/SwipingSessionsController.java
@@ -1,10 +1,13 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import play.Logger;
 import play.mvc.*;
 import models.SwipingSession;
 import models.User;
 
-import static play.libs.Json.toJson;
+import java.util.List;
 
 @Security.Authenticated(Secured.class)
 public class SwipingSessionsController extends Controller {
@@ -23,12 +26,43 @@ public class SwipingSessionsController extends Controller {
             return badRequest();
         }
 
-        SwipingSession swipingSession = SwipingSession.find(initiatorEmail, buddyEmail);
+        List<SwipingSession> swipingSessions = SwipingSession.find(initiatorEmail, buddyEmail);
 
-        if (swipingSession == null) {
+        if (swipingSessions == null || swipingSessions.isEmpty()) {
             return notFound();
         }
 
-        return ok(toJson(swipingSession));
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode json = mapper.valueToTree(swipingSessions);
+
+        return ok(json);
+
+    }
+
+    public Result createSwipingSession(String initiatorEmail, String buddyEmail) {
+
+        if (User.findByEmailAddress(initiatorEmail) == null || User.findByEmailAddress(buddyEmail) == null) {
+
+            /*
+             * The reasoning behind returning a 400 here, rather than a 404, is that a non-existant
+             * user really must be the result of some client-side programming error. Firstly, since authentication
+             * is required at least one of the users have to exist. Secondly, the only time this endpoint
+             * should be accessed is if there has been some indication that a swiping session has been initialized
+             * by another user. If you do not know what email address that user has you are using the api wrongly.
+             */
+            return badRequest();
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode array = mapper.createArrayNode();
+
+        array.add("Moderna Museet");
+
+        return ok(array);
+
+    }
+
+    public Result chooseActivities() {
+        return null;
     }
 }
--- test/SwipingSessionsControllerTest.java
+++ test/SwipingSessionsControllerTest.java
@@ -2,37 +2,35 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import models.SwipingSession;
 import org.junit.Test;
-import org.junit.Before;
 
+import play.Logger;
 import play.libs.Json;
 import play.mvc.Result;
 import play.mvc.Http;
-import play.test.WithApplication;
 import testResources.BaseTest;
-import utils.DemoData;
 
-import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.*;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.*;
 
-import play.Logger;
-
 public class SwipingSessionsControllerTest extends BaseTest {
 
-    //DemoData demoData;
+    /*
+     * Utility methods.
+     */
 
-    /*@Before
-    public void setup() {
-        demoData = app.injector().instanceOf(DemoData.class);
-    }*/
-
-    private Result makeAuthenticatedRequest(String authToken, String email1, String email2) {
+    private Result makeAuthenticatedGetRequest(String authToken, String email1, String email2) {
 
         Http.RequestBuilder fakeRequest = fakeRequest(controllers.routes.SwipingSessionsController.getSwipingSession(email1, email2));
         fakeRequest.header(SecurityController.AUTH_TOKEN_HEADER, authToken);
@@ -41,32 +39,55 @@ public class SwipingSessionsControllerTest extends BaseTest {
 
     }
 
-    private Result makeRequestWithCorrectEmails() {
+    private Result makeGetRequestWithCorrectEmails() {
+        return makeGetRequest(user1Email, user2Email);
+    }
 
-        //String authToken = demoData.user1.createToken();
+    private Result makeGetRequest(String email1, String email2) {
+
         String authToken = user1.createToken();
-        return makeAuthenticatedRequest(authToken, user1Email, user2Email);
+        return makeAuthenticatedGetRequest(authToken, email1, email2);
 
     }
 
-    private Result makeRequest(String email1, String email2) {
+    private Result makeAuthenticatedPostRequest(String authToken, String email1, String email2) {
 
-        String authToken = user1.createToken();
-        return makeAuthenticatedRequest(authToken, email1, email2);
+        Http.RequestBuilder fakeRequest = fakeRequest(controllers.routes.SwipingSessionsController.createSwipingSession(email1, email2));
+        fakeRequest.header(SecurityController.AUTH_TOKEN_HEADER, authToken);
+
+        return route(fakeRequest);
 
     }
+
+    private Result makePostRequest(String email1, String email2) {
+
+        String authToken = user1.createToken();
+        return makeAuthenticatedPostRequest(authToken, email1, email2);
+
+    }
+
+    private Result makePostRequestWithCorrectEmails() {
+        return makePostRequest(user1Email, user2Email);
+    }
+
+    /*
+     * Test GET.
+     */
 
     @Test
     public void getSwipingSession() {
 
-        Result result = makeRequestWithCorrectEmails();
+        Result result = makeGetRequestWithCorrectEmails();
         assertEquals(OK, result.status());
 
         JsonNode json = Json.parse(contentAsString(result));
 
-        assertNotNull(json.get("id"));
-        assertNotNull(json.get("initializationDate"));
+        for (JsonNode swipingSession : json) {
 
+            assertNotNull(swipingSession.get("id"));
+            assertNotNull(swipingSession.get("initializationDate"));
+
+        }
     }
 
     @Test
@@ -81,7 +102,7 @@ public class SwipingSessionsControllerTest extends BaseTest {
     @Test
     public void getSwipingSessionOnNonExistantInitializer() {
         
-        Result result = makeRequest("nonexistant@demo.com", user2Email);
+        Result result = makeGetRequest("nonexistant@demo.com", user2Email);
         assertEquals(BAD_REQUEST, result.status());
 
     }
@@ -89,7 +110,7 @@ public class SwipingSessionsControllerTest extends BaseTest {
     @Test
     public void getSwipingSessionOnNonExistantBuddy() {
         
-        Result result = makeRequest(user1Email, "nonexistant@demo.com");
+        Result result = makeGetRequest(user1Email, "nonexistant@demo.com");
         assertEquals(BAD_REQUEST, result.status());
 
     }
@@ -97,7 +118,7 @@ public class SwipingSessionsControllerTest extends BaseTest {
     @Test
     public void getSwipingSessionOnNullInitializer() {
         
-        Result result = makeRequest(null, user2Email);
+        Result result = makeGetRequest(null, user2Email);
         assertEquals(BAD_REQUEST, result.status());
 
     }
@@ -105,7 +126,7 @@ public class SwipingSessionsControllerTest extends BaseTest {
     @Test
     public void getSwipingSessionOnNullBuddy() {
         
-        Result result = makeRequest(user1Email, null);
+        Result result = makeGetRequest(user1Email, null);
         assertEquals(BAD_REQUEST, result.status());
 
     }
@@ -113,8 +134,90 @@ public class SwipingSessionsControllerTest extends BaseTest {
     @Test
     public void getSwipingSessionOnNonExistantSwipingSession() {
 
-        Result result = makeRequest(user2Email, user1Email);
+        Result result = makeGetRequest(user2Email, user1Email);
         assertEquals(NOT_FOUND, result.status());
         
+    }
+
+    /*
+     * Test POST.
+     */
+
+    @Test
+    public void testInitiateSwipingSessionWhenNotLoggedIn() {
+
+        Result result = route(fakeRequest(controllers.routes.SwipingSessionsController.createSwipingSession(user1Email,
+                user2Email)));
+        assertEquals(UNAUTHORIZED, result.status());
+
+    }
+
+    @Test
+    public void testInitiateSwipingSessionOnIncorrectEmailsButCorrectSession() {
+
+        Result result = makePostRequest("nonexistant1@demo.com", "nonexistant2@demo.com");
+        assertEquals(BAD_REQUEST, result.status());
+
+    }
+
+    @Test
+    public void testInitiateSwipingSessionReturnsOkOnCorrectCredentials() {
+
+        Result result = makePostRequestWithCorrectEmails();
+        assertEquals(OK, result.status());
+
+    }
+
+    @Test
+    public void testInitiateSwipingSessionCreatesSwipingSession() {
+
+        List<SwipingSession> before = SwipingSession.find(user1Email, user2Email);
+
+        makePostRequestWithCorrectEmails();
+
+        List<SwipingSession> after = SwipingSession.find(user1Email, user2Email);
+
+        assertTrue(after.size() == (before.size() + 1));
+
+    }
+
+    @Test
+    public void testInitiateSwipingSessionReturnsNonEmptyArray() {
+
+        Result result = makePostRequestWithCorrectEmails();
+
+        JsonNode json = Json.parse(contentAsString(result));
+        assertNotNull(json);
+
+        ObjectMapper mapper = new ObjectMapper();
+        TypeFactory typeFactory = mapper.getTypeFactory();
+
+        try {
+
+            List<String> activities = mapper.readValue(contentAsString(result), typeFactory.constructCollectionType(List.class, String.class));
+
+            assertNotNull(activities);
+            assertTrue(activities.size() > 0);
+
+        } catch (IOException e) {
+            fail("IOException when creating activity list from response");
+        }
+    }
+
+    /*
+     * Test PUT.
+     */
+
+    @Test
+    public void testChooseActivitiesWhenNotLoggedIn() {
+
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode array = mapper.createArrayNode();
+
+        array.add("Moderna Museet");
+
+        Result result = route(fakeRequest(controllers.routes.SwipingSessionsController.chooseActivities()));
+        assertEquals(UNAUTHORIZED, result.status());
+
     }
 }
