21ed680a74aae5a08168daa06cfe61206ef020a3
Simon Olofsson <s.olofsson@gmail.com>
2017-04-21 16:58:23 +0200

    Updated javadocs.

--- app/models/SwipingSession.java
+++ app/models/SwipingSession.java
@@ -6,6 +6,13 @@ import javax.persistence.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * An entity representing a swiping session between a set of users. Apart from
+ * having a set of users associated with it, each swiping session also keeps
+ * track of their chosen activities.
+ *
+ * @author Simon Olofsson
+ */
 @Entity
 public class SwipingSession extends Model {
 
@@ -31,6 +38,13 @@ public class SwipingSession extends Model {
 
     }
 
+    /**
+     * Method to record a user's choice of activities.
+     *
+     * @param userEmailAddress the email address of the user whose choice is to
+     *                         be recorded.
+     * @param activities the activities to associate with the user.
+     */
     public void setUserActivityChoice(String userEmailAddress, List<Activity> activities) {
 
         User user = User.findByEmailAddress(userEmailAddress);
@@ -41,10 +55,28 @@ public class SwipingSession extends Model {
 
     }
 
+    /**
+     * Method to get the activities chosen by a user during this swiping session.
+     *
+     * @param userEmailAddress the email address of the user whose activities
+     *                         are to be returned.
+     * @return a list containing the activities chosen by the user,
+     * may be empty.
+     */
     public List<Activity> getChosenActivities(String userEmailAddress) {
         return ActivityChoice.findBySwipingSessionAndUser(User.findByEmailAddress(userEmailAddress).id, id).activities;
     }
 
+    /**
+     * Method to get all swiping sessions where all of the users and exactly
+     * those users associate with the passed email addresses have taken part.
+     * That is, will only return those swiping sessions whose set of users
+     * are exactly equal to those indicated by the email addresses.
+     *
+     * @param emails a list of emails to match swiping sessions against.
+     * @return a list of swiping sessions that are associated with exactly
+     * those users indicated by the email addresses passed.
+     */
     public static List<SwipingSession> findByEmail(List<String> emails) {
 
         Set<User> users = new HashSet<>();
@@ -57,6 +89,12 @@ public class SwipingSession extends Model {
 
     }
 
+    /**
+     * Method to find a swiping session by its id.
+     *
+     * @param id the id of the swiping session to get.
+     * @return the swiping session indicated by the id, null if there is none.
+     */
     public static SwipingSession findById(long id) {
         return find.byId(id);
     }
--- app/utils/ActivityGenerator.java
+++ app/utils/ActivityGenerator.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Utility class for generating activities to pass to users.
+ *
+ * @author Simon Olofsson
+ */
 public class ActivityGenerator {
 
     private static List<String> generateRandomActivities() {
@@ -22,6 +27,14 @@ public class ActivityGenerator {
 
     }
 
+    /**
+     * Method to generate activities as a Jackson ArrayNode. Takes an
+     * ObjectNode, creates an ArrayNode on it and puts the generated
+     * activities in it. As such, it modifies the ObjectNode passed
+     * and returns nothing.
+     *
+     * @param json a Jackson ObjectNode to put the ArrayNode in.
+     */
     public static void generateActivitiesAsArrayNode(ObjectNode json) {
 
         ArrayNode array = json.putArray("activities");
