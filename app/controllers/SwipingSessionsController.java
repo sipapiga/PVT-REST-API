acb50776208de514bf15de18bb23cec12d278ddf
Simon Olofsson <s.olofsson@gmail.com>
2017-04-21 15:42:24 +0200

    Removed commented out code.

--- app/controllers/SwipingSessionsController.java
+++ app/controllers/SwipingSessionsController.java
@@ -23,20 +23,6 @@ public class SwipingSessionsController extends Controller {
 
     public Result getSwipingSession(String emailAddresses) {
 
-        //if (User.findByEmailAddress(initiatorEmail) == null || User.findByEmailAddress(buddyEmail) == null) {
-
-            /*
-             * The reasoning behind returning a 400 here, rather than a 404, is that a non-existant
-             * user really must be the result of some client-side programming error. Firstly, since authentication
-             * is required at least one of the users have to exist. Secondly, the only time this endpoint
-             * should be accessed is if there has been some indication that a swiping session has been initialized
-             * by another user. If you do not know what email address that user has you are using the api wrongly.
-             */
-            //return badRequest();
-        //}
-
-        //List<SwipingSession> swipingSessions = SwipingSession.findByEmail(initiatorEmail, buddyEmail);
-
         ObjectMapper mapper = new ObjectMapper();
 
         try {
@@ -123,18 +109,6 @@ public class SwipingSessionsController extends Controller {
             jsonActivities.forEach(activityName -> parsedActivities.add(Activity.findByName(activityName.asText())));
 
             SwipingSession swipingSession = SwipingSession.findById(swipingSessionId);
-
-            /*long userId = User.findByEmailAddress(email).id;
-
-            if (userId == swipingSession.initiator.id) {
-                swipingSession.initiatorActivities = parsedActivities;
-            } else if(userId == swipingSession.buddy.id) {
-                swipingSession.buddyActivities = parsedActivities;
-            } else {
-                return buildBadRequestResponse(mapper,
-                        "The email address passed does not match any of the participants in the swiping session.");
-            }*/
-
 
             try {
 
