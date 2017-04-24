9d295a58241eac98619b947c94e421fd63df8860
Simon Olofsson <s.olofsson@gmail.com>
2017-04-24 12:10:53 +0200

    Created ActivityGatherer class.

---
+++ app/utils/ActivityGatherer.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ * Gathers data from API's and saves them to the database.
+ */
+public class ActivityGatherer {
+
+    /**
+     * Executes gathering of data.
+     */
+    public void gather() {
+
+    }
+}
