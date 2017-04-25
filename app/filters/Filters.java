efb147d9c148e3a8d34d0f2b1d202a5c1a151d93
Simon Olofsson <s.olofsson@gmail.com>
2017-04-25 19:59:41 +0200

    Removed unused, commented out code.

--- app/filters/Filters.java
+++ app/filters/Filters.java
@@ -1,53 +1,12 @@
 package filters;
 
-import org.springframework.beans.factory.annotation.Required;
-import play.Logger;
 import play.filters.cors.CORSFilter;
 import play.http.DefaultHttpFilters;
-import play.http.HttpFilters;
-import play.mvc.EssentialAction;
-import play.mvc.EssentialFilter;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
 public class Filters extends DefaultHttpFilters {
     @Inject public Filters(CORSFilter corsFilter, LoggingFilter loggingFilter) {
         super(corsFilter, loggingFilter);
     }
 }
-
-/*public class Filters implements HttpFilters {
-
-    @Inject
-    CORSFilter corsFilter;
-
-    @Inject
-    LoggingFilter loggingFilter;
-
-    public EssentialFilter[] filters() {
-        return new EssentialFilter[] {corsFilter.asJava(), loggingFilter.asJava()};
-    }
-}*/
-
-/*@Singleton
-public class Filters extends EssentialFilter implements HttpFilters {
-
-    @Inject
-    private CORSFilter corsFilter;
-
-    @Override
-    public EssentialAction apply(EssentialAction next) {
-        return corsFilter.asJava().apply(next);
-    }
-
-    @Override
-    public EssentialFilter[] filters() {
-
-        EssentialFilter[] result = new EssentialFilter[1];
-        result[0] = this;
-
-        return result;
-
-    }
-}*/
