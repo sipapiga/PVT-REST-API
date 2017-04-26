77b2302a914ad932f9eb5801178a0fac8fa5de33
Simon Olofsson <s.olofsson@gmail.com>
2017-04-26 07:28:18 +0200

    Removed unnecessary debug logging.

--- public/javascripts/js-client.js
+++ public/javascripts/js-client.js
@@ -1,5 +1,4 @@
-//let SERVER = 'http:/localhost:9000';
-let SERVER = 'https://protected-gorge-44302.herokuapp.com';
+let SERVER = 'http://localhost:9000';
 
 // From http://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
 let cookies;
@@ -90,8 +89,6 @@ let facebookLogin = function(facebookToken) {
 
     });
 };
-
-facebookLogin('EAACEdEose0cBAAn2cNtXXROPzalQo8GZA7MlsKvYHEMpQq1SiJcnxGjJl01ZBDHpE6JZBVCB32XJGzkjBRfSrXjhQl4bB0ZCxmjKpivRZC92rZASsxvQ5rKGy1rHTzKM1ZC3WZCr1uiKldosyHKx8ZA9leWTJC7TZB4R1jLf3ZBoS5WUSJWbDRs44BH');
 
 /*
  * 1. Login.
