22788e5d5b137ea5df45c92a6434542bdd58a1f3
Simon Olofsson <s.olofsson@gmail.com>
2017-04-26 07:05:16 +0200

    Removed CORS filter for experimental purposes.

--- app/filters/Filters.java
+++ app/filters/Filters.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 public class Filters extends DefaultHttpFilters {
     @Inject public Filters(CORSFilter corsFilter, LoggingFilter loggingFilter) {
-        super(corsFilter, loggingFilter);
+        //super(corsFilter, loggingFilter);
+        super(loggingFilter);
     }
 }
