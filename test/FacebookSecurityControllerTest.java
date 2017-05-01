5770775c8ee94dc6e1a713e8da3a87a4cc655e89
Simon Olofsson <s.olofsson@gmail.com>
2017-05-01 11:59:00 +0200

    Now passing facebook authentication token in request body instead of custom header.

--- test/FacebookSecurityControllerTest.java
+++ test/FacebookSecurityControllerTest.java
@@ -1,8 +1,18 @@
 package controllers;
 
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Test;
 import play.mvc.Result;
 import testResources.BaseTest;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -13,12 +23,31 @@ import static play.test.Helpers.route;
 
 public class FacebookSecurityControllerTest extends BaseTest {
 
-    /*@Test
+    ObjectMapper mapper = new ObjectMapper();
+
+    private JsonNode createRequestBody(String facebookToken) {
+
+        ObjectNode bodyJson = mapper.createObjectNode();
+        bodyJson.put("facebookAuthToken", facebookToken);
+
+        return bodyJson;
+
+    }
+
+    private JsonNode createBodyWithInvalidFacebookToken() {
+        return createRequestBody("");
+    }
+
+    private JsonNode createBodyWithValidFacebookToken() {
+        return createRequestBody(facebookToken);
+    }
+
+    @Test
     public void testLogin() {
 
         if (facebookToken != null) {
 
-            Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).header(FacebookSecurityController.AUTH_TOKEN_HEADER, facebookToken));
+            Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(createBodyWithValidFacebookToken()));
             assertEquals(OK, result.status());
 
         } else {
@@ -33,10 +62,10 @@ public class FacebookSecurityControllerTest extends BaseTest {
 
         if (facebookToken != null) {
 
-            Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).header(FacebookSecurityController.AUTH_TOKEN_HEADER, facebookToken));
+            Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(createBodyWithValidFacebookToken()));
             assertEquals(OK, result.status());
 
-            result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).header(FacebookSecurityController.AUTH_TOKEN_HEADER, facebookToken));
+            result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(createBodyWithValidFacebookToken()));
             assertEquals(OK, result.status());
 
         } else {
@@ -44,12 +73,12 @@ public class FacebookSecurityControllerTest extends BaseTest {
                     "Please specify one in 'conf/application.secrets.conf' under the key 'facebookToken'." +
                     "Valid access tokens can be obtained from https://developers.facebook.com/tools/explorer");
         }
-    }*/
+    }
 
     @Test
     public void testLoginOnEmptyAccessToken() {
 
-        Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).header(FacebookSecurityController.AUTH_TOKEN_HEADER, ""));
+        Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(createBodyWithInvalidFacebookToken()));
         assertEquals(BAD_REQUEST, result.status());
 
     }
@@ -57,7 +86,27 @@ public class FacebookSecurityControllerTest extends BaseTest {
     @Test
     public void testLoginOnInvalidAccessToken() {
 
-        Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).header(FacebookSecurityController.AUTH_TOKEN_HEADER, "invalid"));
+        Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(createBodyWithInvalidFacebookToken()));
+        assertEquals(BAD_REQUEST, result.status());
+
+    }
+
+    @Test
+    public void testLoginOnEmptyRequestBody() {
+
+        ObjectNode bodyJson = mapper.createObjectNode();
+
+        Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(bodyJson));
+        assertEquals(BAD_REQUEST, result.status());
+
+    }
+
+    @Test
+    public void testLoginOnNullRequestBody() {
+
+        ObjectNode bodyJson = null;
+
+        Result result = route(fakeRequest(controllers.routes.FacebookSecurityController.login()).bodyJson(bodyJson));
         assertEquals(BAD_REQUEST, result.status());
 
     }
